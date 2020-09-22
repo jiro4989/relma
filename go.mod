511144ddd05a18597a24b83f8125192f0c44766d
jiro4989 <jiroron666@gmail.com>
2020-09-22 19:29:39 +0900

    :tada: initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jiro4989/ghrpkg
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+const usage = `ghrpkg manages GitHub Releases versioning.
+
+Usage:
+  ghrpkg [commands] [options]
+  ghrpkg -h | --help
+  ghrpkg --version
+
+Examples:
+  $ ghrpkg init
+
+  $ ghrpkg edit
+
+  $ ghrpkg install https://github.com/jiro4989/nimjson/releases/download/v1.2.6/nimjson_linux.tar.gz
+
+  $ ghrpkg list --upgradable
+
+  $ ghrpkg show jiro4989/nimjson
+
+  $ ghrpkg upgrade jiro4989/nimjson v1.2.7
+
+  $ ghrpkg upgrade --all
+
+  $ ghrpkg remove jiro4989/nimjson
+
+Commands:
+  init
+  install [-d | --dry-run] <github_release_url>
+  list    [-u | --upgradable]
+  upgrade show <package>
+  upgrade <package> <version>
+          [--all]
+  remove <package>
+  edit
+
+Options:
+  -h, --help    print this help
+      --version print version
+`
